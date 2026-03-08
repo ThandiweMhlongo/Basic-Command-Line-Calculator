52b1beefd8a6fb523d1a20bd5b5892a1d086ad0f
ThandiweMhlongo <128644984+ThandiweMhlongo@users.noreply.github.com>
2026-03-08 14:00:18 +0200

    feature: initialize main method and first operand input.

--- src/calculator.java
+++ src/calculator.java
@@ -1,2 +1,11 @@
+import java.util.Scanner;
+
 public class calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Java Calculator");
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+    }
 }
